d5a724af478f6e184517a7ae2782f2f01d6141d9
nurr <nurr@pop-os.localdomain>
2023-06-24 18:57:54 +0700

    fix responsive Footer Component

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -11,19 +11,25 @@ import GooglePlay from "/img/GooglePlay.svg";
 function Footer() {
    return (
       <>
-         <div className='px-5 text-white bg-violet-950 py-9'>
-            <div>
+         <div className='px-5 text-white sm:grid sm:grid-cols-2 bg-violet-950 py-9'>
+            <div className='md:grid md:grid-rows-3 md:items-center'>
                <div>
                   <img src={KoinImage} alt='' />
                </div>
-               <div className='grid grid-rows-4 gap-4 mt-4 text-xl'>
+               <div className='grid grid-rows-4 gap-4 text-xl md:text-md md:gap-0 md:grid-rows-none lg:grid-rows-none md:grid-cols-4 md:items-center md:justify-center'>
                   <Link to='/'>FAQ's</Link>
                   <Link to='/'>Contact Us</Link>
                   <Link to='/'>Terms & Conditions</Link>
                   <Link to='/'>Privacy Policy</Link>
                </div>
+               <div className='items-center hidden gap-5 my-6 sm:flex'>
+                  <img src={Instagram} alt='' />
+                  <img src={Facebook} alt='' />
+                  <img src={Twitter} alt='' />
+                  <img src={Linkedin} alt='' />
+               </div>
             </div>
-            <div className='mt-4'>
+            <div className='items-center mt-4 md:grid md:grid-rows-2 md:justify-center'>
                <form className=''>
                   <label className='text-xl ' htmlFor='email'>
                      Join our Mailing List
@@ -40,13 +46,13 @@ function Footer() {
                      </button>
                   </div>
                </form>
-               <div className='flex items-center gap-5 my-6'>
+               <div className='flex items-center gap-5 my-6 sm:hidden'>
                   <img src={Instagram} alt='' />
                   <img src={Facebook} alt='' />
                   <img src={Twitter} alt='' />
                   <img src={Linkedin} alt='' />
                </div>
-               <div className='flex items-center justify-center gap-1 px-20'>
+               <div className='flex justify-center gap-1 px-20 md:px-1 md:pt-0 items-cente sm:pt-4'>
                   <img src={AppStore} alt='' />
                   <img src={GooglePlay} alt='' />
                </div>
