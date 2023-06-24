01ae512ddd6eb708629c7eeb50f9300cdbc8ec5d
nurr <nurr@pop-os.localdomain>
2023-06-24 10:02:05 +0700

    fix responsive Download Component

--- src/Component/Download/Download.jsx
+++ src/Component/Download/Download.jsx
@@ -6,16 +6,23 @@ import Mobile from "/img/Mobile.svg";
 function Dowload() {
    return (
       <>
-         <div className='flex flex-col items-center py-5 font-poppins px-7 bg-violet-950'>
-            <h1 className='text-3xl font-semibold text-white'>
-               More Your Control for Your Money
-            </h1>
-            <div className='flex justify-center gap-2 py-3'>
-               <img src={AppStore} alt='' />
-               <img src={GooglePlay} alt='' />
+         <div className='flex flex-col items-center py-5 md:justify-between md:flex-row md:items-start font-poppins px-7 bg-violet-950'>
+            <div className='flex flex-col '>
+               <h1 className='text-3xl font-semibold text-white'>
+                  More Your Control for Your Money
+               </h1>
+               <div className='flex justify-center gap-2 py-3 md:justify-start'>
+                  <img src={AppStore} alt='' />
+                  <img src={GooglePlay} alt='' />
+               </div>
             </div>
-            <div>
-               <img src={Mobile} alt='' />
+
+            <div className='md:absolute md:left-36 '>
+               <img
+                  className='md:relative md:ml-96 md:bottom-64'
+                  src={Mobile}
+                  alt=''
+               />
             </div>
          </div>
       </>
