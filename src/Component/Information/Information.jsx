c57472f9ec40a7ad441a652b1d3b12445f90dedf
nurr <nurr@pop-os.localdomain>
2023-06-25 17:23:19 +0700

    fix bg color in Information Component

--- src/Component/Information/Information.jsx
+++ src/Component/Information/Information.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Information() {
    return (
       <>
-         <div className='relative flex flex-col items-center justify-between px-20 py-10 mx-5 shadow-md bg-violet-400 md:mx-10 sm:gap-5 sm:py-3 sm:flex-row shadow-violet-300 rounded-xl bottom-9'>
+         <div className='relative flex flex-col items-center justify-between px-20 py-10 mx-5 bg-gray-200 shadow-md md:mx-10 sm:gap-5 sm:py-3 sm:flex-row shadow-gray-400 rounded-xl bottom-9'>
             <div className='flex flex-col items-center'>
                <h2 className='text-4xl font-semibold '>
                   10K<span className='text-violet-900'>+</span>
