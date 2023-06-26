ff93e266bc169166f1c2f83ada7699401f95862b
nurr <nurrohmanmarduki@gmail.com>
2023-06-26 18:20:05 +0700

    fix text position in ErrorPage Component

--- src/Component/ErrorPage.jsx
+++ src/Component/ErrorPage.jsx
@@ -8,14 +8,16 @@ export default function ErrorPage() {
    return (
       <div
          id='error-page'
-         className='flex flex-col items-center justify-center h-screen'
+         className='flex flex-col items-center justify-center h-screen px-5'
       >
          <div>
             <img className='w-96 h-96' src={NotFoundImage} alt='' />
          </div>
-         <p className='pt-2 font-semibold font-poppins'>
-            Maaf halaman yang anda cari belum dibuat
-         </p>
+         <div>
+            <p className='pt-2 font-semibold text-center font-poppins'>
+               maaf halaman yang anda cari belum dibuat
+            </p>
+         </div>
       </div>
    );
 }
