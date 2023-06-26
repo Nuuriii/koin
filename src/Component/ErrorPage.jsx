1211c92305b5747e45dccb33fb9129889cf6c086
nurr <nurrohmanmarduki@gmail.com>
2023-06-26 12:50:13 +0700

    add Ui for ErrorPage

--- src/Component/ErrorPage.jsx
+++ src/Component/ErrorPage.jsx
@@ -1,15 +1,20 @@
 import { useRouteError } from "react-router-dom";
+import NotFoundImage from "/img/NotFound.svg";
 
 export default function ErrorPage() {
-   const error = useRouteError();
+   /* const error = useRouteError();
    console.error(error);
-
+ */
    return (
-      <div id='error-page'>
-         <h1>Oops!</h1>
-         <p>Sorry, an unexpected error has occurred.</p>
-         <p>
-            <i>{error.statusText || error.message}</i>
+      <div
+         id='error-page'
+         className='flex flex-col items-center justify-center h-screen'
+      >
+         <div>
+            <img src={NotFoundImage} alt='' />
+         </div>
+         <p className='pt-4 font-semibold font-poppins'>
+            Maaf halaman yang anda cari belum dibuat
          </p>
       </div>
    );
