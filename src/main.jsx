8da41eb0865a0e54aa1040694ed3b45f1b2a0af6
nurr <nurrohmanmarduki@gmail.com>
2023-06-26 12:44:04 +0700

    create ErrorPage Component and add NotFound Image

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LandingPage from "./Component/LandingPage";
+import ErrorPage from "./Component/ErrorPage";
 import "./index.css";
 
 const router = createBrowserRouter([
    {
       path: "/",
       element: <LandingPage />,
+      errorElement: <ErrorPage />,
    },
 ]);
 
