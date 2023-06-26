9e3c6b28b74e92194552c55323bd051a46b16511
nurr <nurrohmanmarduki@gmail.com>
2023-06-26 07:51:59 +0700

    fix responsive in Download Component

--- src/Component/Download/Download.jsx
+++ src/Component/Download/Download.jsx
@@ -6,23 +6,19 @@ import Mobile from "/img/Mobile.svg";
 function Dowload() {
    return (
       <>
-         <div className='flex flex-col items-center py-5 md:justify-between md:flex-row md:items-start font-poppins px-7 bg-violet-950'>
+         <div className='flex flex-col items-center py-5 md:justify-between md:flex-row md:items-cent md:h-56 font-poppins px-7 bg-violet-950'>
             <div className='flex flex-col '>
                <h1 className='text-3xl font-semibold text-white'>
                   More Your Control for Your Money
                </h1>
-               <div className='flex justify-center gap-2 py-3 md:justify-start'>
+               <div className='flex justify-center gap-2 py-3 md:justify-start md:items-start'>
                   <img src={AppStore} alt='' />
                   <img src={GooglePlay} alt='' />
                </div>
             </div>
 
-            <div className='md:absolute md:left-36 '>
-               <img
-                  className='md:relative md:ml-96 md:bottom-64'
-                  src={Mobile}
-                  alt=''
-               />
+            <div className='md:relative bottom-20 '>
+               <img className='' src={Mobile} alt='' />
             </div>
          </div>
       </>
